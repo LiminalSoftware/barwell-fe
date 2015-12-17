ceaa57e46e5f1cdeb5528a3a47cd2fbd71781e0d
Peter Rauchfuss <p.rauchfuss@gmail.com>
2015-12-16 20:03:50 -0500

    detail bars

--- app/containers/Views/Fields/DateField/detail.jsx
+++ app/containers/Views/Fields/DateField/detail.jsx
@@ -30,11 +30,26 @@ var DateDetail = React.createClass({
 		this.setState({year: this.state.year - 1})
 	},
 
+	getDaysInYear: function (center) {
+		var weeks = []
+		var start = moment(center).subtract(moment(center).day() + 26*7, 'days')
+		for (var w = 0; w < 52; w++) {
+			var days = []
+			for (var d = 0; d < 7; d++) {
+				days.push(start.clone().add(w * 7 + d, 'days'))
+			}
+			weeks.push(days)
+		}
+		return weeks
+	},
+
 	render: function() {
+		var value = this.state.date
     var model = this.props.model
     var view = this.props.view
 		var obj = this.props.object
 		var config = this.props.config
+		var weeks = this.getDaysInYear(value)
 
 		return <div className = "color-detail-inner">
 			<div className = "header-label">Date picker</div>
@@ -53,6 +68,26 @@ var DateDetail = React.createClass({
 				<div className="dropdown small grayed icon icon-geo-arrw-right" onClick = {this.incMonth}></div>
 			</div>
 			<div className = "color-detail-scroll">
+				<table className = "date-detail-week"><tbody>
+					{weeks.map(function (days) {
+						return <tr className = "week-box">
+							{days.map(function (day, idx) {
+								var classes = []
+								var isLastWeek = (day.clone().add(1, 'week').month() !== day.month())
+								var isLastDay = (day.clone().add(1, 'day').month() !== day.month())
+								classes.push('day-box')
+								if (isLastWeek) classes.push('last-week')
+								if (isLastDay || idx === 6) classes.push('last-day')
+								if (idx === 0) classes.push('first-day')
+								if (day.isSame(value)) classes.push('selected')
+								return <td className = {classes.join(' ')}>
+									{day.date()}
+								</td>
+							})}
+						</tr>
+					})
+					}
+				</tbody></table>
 			</div>
 		</div>
 	}
