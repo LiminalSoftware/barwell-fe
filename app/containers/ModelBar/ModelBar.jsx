7e84a39a7260935b637b188acfdf65fbcf8310a2
Peter Rauchfuss <p.rauchfuss@gmail.com>
2017-01-07 18:37:19 -0500

    Formatting bug

--- app/containers/ModelBar/ModelBar.jsx
+++ app/containers/ModelBar/ModelBar.jsx
@@ -91,7 +91,7 @@ class ModelBar extends Component {
 
 		return <div className="mdlbar" onClick = {this.focus} style={style}>
 			<h1 className="branding" style={{display: "flex"}}>
-				<span>metasheet</span> ``
+				<span>metasheet</span>
 			</h1>
 
 			<div className="mdlbar-list" ref="outer" onWheel={this.handleMouseWheel}>
